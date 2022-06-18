67f0c7f8b6181e8ef4f60b573e365857c8174946
Nick Shaydayuk <ghost2394228@gmail.com>
2022-06-18 04:39:46 +0300

    исправить ошибки в стилях, разметке и логике

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let profile = document.querySelector(".profile");
 let card = document.querySelector(".card");
-let like = card.querySelector(".card__likeButton");
+/* let like = card.querySelector(".card__like-button"); */
 let popup = document.querySelector(".popup");
-let submitButton = popup.querySelector(".popup__submitButton");
-let closeButton = document.querySelector(".popup__closeButton");
+let popupForm = popup.querySelector(".popup__form")
+let closeButton = document.querySelector(".popup__close-button");
 let editButton = document.querySelector(".profile__edit-button");
 let profileName = profile.querySelector(".profile__name");
 let profileText = profile.querySelector(".profile__text");
@@ -12,35 +12,35 @@ let inputProfileText = popup.querySelector("#text");
 
 function insertName(profileName) {
   inputProfileName.value = profileName;
-};
+}
 function insertText(profileText) {
   inputProfileText.value = profileText;
-};
-insertName(profileName.textContent);
-insertText(profileText.textContent);
+}
 
 function renderEdition() {
   popup.classList.toggle("popup_active");
-};
+}
 
 function editName(evt) {
-    evt.preventDefault();
-    profileName.textContent = inputProfileName.value
-    profileText.textContent = inputProfileText.value
-    renderEdition();
-};
+  evt.preventDefault();
+  profileName.textContent = inputProfileName.value;
+  profileText.textContent = inputProfileText.value;
+  renderEdition();
+}
 
 function popupOpen() {
   popup.classList.toggle("popup_active");
-};
+  insertName(profileName.textContent);
+  insertText(profileText.textContent);
+}
 function popupClose() {
   popup.classList.toggle("popup_active");
-};
+}
 
 function activeLike() {
-    like.classList.toggle("card__likeButton_active")
+  like.classList.toggle("card__likeButton_active");
 }
 editButton.addEventListener("click", popupOpen);
 closeButton.addEventListener("click", popupClose);
-submitButton.addEventListener("click", editName);
-like.addEventListener("click", activeLike)
+popupForm.addEventListener("submit", editName);
+like.addEventListener("click", activeLike);
